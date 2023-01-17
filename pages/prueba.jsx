ff400f5be09b7709c19d70e74e9c3f6ce11b5701
Christian Soto <christiansoto@folio3.com>
2023-01-17 10:36:22 -0600

    Fix chatbar missing import

--- pages/prueba.jsx
+++ pages/prueba.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
-import ChatBar from "../Components/ChatBar";
 function prueba() {
   return (
     <div>
-      <ChatBar />
       <NavBar />
       <div>
         <h1>dasd</h1>
