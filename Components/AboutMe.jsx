0c9e6e8675ba4a8f07995736ed5cb278ed0bb0ec
Christian Soto <syasdras@gmail.com>
2023-05-04 05:46:24 -0600

    Navbar arreglada

--- Components/AboutMe.jsx
+++ Components/AboutMe.jsx
@@ -3,19 +3,20 @@ import Button from '@mui/material/Button';
 import ExpandCircleDownOutlinedIcon from '@mui/icons-material/ExpandCircleDownOutlined';
 
 function AboutMe({ user }) {
+  console.log('sssss', user);
   return (
     <div className="aboutMe-container">
       {user.experience && (
         <div className="aboutMe-experience">
           <span className="aboutMe-experience-title title">Experiencia</span>
-          <p className="aboutMe-experience-content text"> {user.experience} </p>
+          <p style={{color: "#dddddd"}} className="aboutMe-experience-content text"> {user.experience} </p>
         </div>
       )}
 
       {user.aboutMe && (
         <div className="aboutMe-me">
           <span className="aboutMe-me-title title">Acerca de mi</span>
-          <p className="aboutMe-me-content text">{user.aboutMe}</p>
+          <p style={{color: "#dddddd"}} className="aboutMe-me-content text">{user.aboutMe}</p>
         </div>
       )}
       {/* <Button
